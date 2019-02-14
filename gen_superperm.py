faa8bba1c59eef7e768fdb4bf3e1f2e2110265f4
Miles Gould <m.gould@zonefox.com>
2019-02-14 11:09:49 +0000

    Represent classes as sets of indices

--- gen_superperm.py
+++ gen_superperm.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
 import itertools
 import math
 import sys
 
+
 n = int(sys.argv[1])
 nfac = math.factorial(n)
+
+def normalise_necklace(p):
+    i = p.index(1)
+    return p[i:] + p[0:i]
+
+perms = list(itertools.permutations(range(1, n + 1)))
+classes = defaultdict(set)
+for (i, p) in enumerate(perms):
+    classes[normalise_necklace(p)].add(i + 1) # MiniZinc arrays are 1-indexed
 
 print(f"n = {n};")
 print(f"pattern_length = {n};")
 print(f"num_patterns = {nfac};")
 print(f"num_classes = {nfac};")
 print("pattern = [|",)
-perms = itertools.permutations(range(1, n + 1))
 perm_strings = (", ".join(map(str, p)) for p in perms)
 print("\n| ".join(perm_strings))
 print("|];");
-print("class = " + str([i for i in range(1, nfac + 1)]) + ";")
+print(f"class = {list(classes.values())};")
